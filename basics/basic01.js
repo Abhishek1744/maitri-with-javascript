985278f138ac3aada6299fc2e8bdee5a33d4ba30
Abhishek1744 <abhishekmujumale1744@gmail.com>
2025-01-05 14:19:52 +0530

    basics of js

---
+++ basics/basic01.js
@@ -0,0 +1,11 @@
+console.log('maitri-with-js')
+
+//Alert statement
+//alert( 3 + 3 )
+
+//No need of semi-colons
+//Code should be readable 
+//tc39  and    MDN documentation would be better for reference
+
+//Null data type is Object
+// Undefined data type is undefined only
---
+++ basics/basics02.js
@@ -0,0 +1,12 @@
+// console.log("2" > 1);
+// console.log(2 > "1");
+
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null > 1);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined < 0);
+
+// always avoid this type of comparasision (null and undefined) due to confusion issues 
